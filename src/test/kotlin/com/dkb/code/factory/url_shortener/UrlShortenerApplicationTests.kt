fa1dbb628fc221e2d88db855ea75b636a7fa71bc
sakshid-email01 <sakshid.email01@gmail.com>
2025-09-25 23:09:03 +0530

    Initial commit

---
+++ src/test/kotlin/com/dkb/code/factory/url_shortener/UrlShortenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dkb.code.factory.url_shortener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
