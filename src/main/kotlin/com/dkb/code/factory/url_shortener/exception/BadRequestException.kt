b1130ab59325900d92139290f16ad7e62e234617
sakshid-email01 <sakshid.email01@gmail.com>
2025-10-01 18:04:38 +0530

    class definition updated with default message

--- src/main/kotlin/com/dkb/code/factory/url_shortener/exception/BadRequestException.kt
+++ src/main/kotlin/com/dkb/code/factory/url_shortener/exception/BadRequestException.kt
@@ -1,9 +1,4 @@
 package com.dkb.code.factory.url_shortener.exception
 
 
-import org.springframework.http.HttpStatus
-
-class BadRequestException(
-    override val message: String,
-    val status: HttpStatus = HttpStatus.BAD_REQUEST
-) : RuntimeException(message)
+class BadRequestException(message: String? = "Invalid Request") : RuntimeException(message)
