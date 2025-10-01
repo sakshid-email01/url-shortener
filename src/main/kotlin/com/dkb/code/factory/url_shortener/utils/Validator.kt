4cb7e81ab26f49d92464524ff23cf2f448be2398
sakshid-email01 <sakshid.email01@gmail.com>
2025-10-01 13:00:07 +0530

    basic validations included, code optimized

--- src/main/kotlin/com/dkb/code/factory/url_shortener/utils/Validator.kt
+++ src/main/kotlin/com/dkb/code/factory/url_shortener/utils/Validator.kt
@@ -1,45 +1,30 @@
 package com.dkb.code.factory.url_shortener.utils
 
 import com.dkb.code.factory.url_shortener.config.AppConfig
+import com.dkb.code.factory.url_shortener.dto.ShortenRequest
 import com.dkb.code.factory.url_shortener.exception.BadRequestException
-import com.fasterxml.jackson.databind.JsonNode
 import org.springframework.stereotype.Component
-import java.net.URI
+
 
 
 @Component
 class Validator (private val appConfig: AppConfig) {
 
-    fun validateShortenRequest(request: JsonNode) {
-        // 1. Null or empty check
-        if (request.isEmpty) {
-            throw BadRequestException("error.empty_request")
-        }
+    fun validateShortenRequest(request: ShortenRequest) {
+        val originalUrl = request.originalUrl
 
-        // 2. Required field check
-        val originalUrlNode = request.get("originalUrl")?.asText()
-        if (originalUrlNode == null || originalUrlNode.isBlank()) {
+        // 1. Null or empty check
+        if (originalUrl.isEmpty() || originalUrl.isBlank()) {
             throw BadRequestException("error.missing_original_url")
         }
 
-        // 3. URL format check
-//        try {
-//            val url = URI.create(originalUrlNode)
-//            if (url.scheme !in listOf("http", "https")) {
-//                throw BadRequestException("error.invalid_protocol")
-//            }
-//        } catch (e: IllegalArgumentException) {
-//            // Covers URISyntaxException wrapped by URI.create
-//            throw BadRequestException("error.invalid_url_format")
-//        }
-
-        // 4. Maximum length check
-        if (originalUrlNode.length > appConfig.urlMaxLength.toInt()) {
+        // 2. Maximum length check
+        if (originalUrl.length > appConfig.urlMaxLength.toInt()) {
             throw BadRequestException("error.url_too_long")
         }
 
-        // 5. Optional: Custom business rules
-        if (originalUrlNode.contains("blocked-domain.com")) {
+        // 3. Optional: Custom business rules
+        if (originalUrl.contains("blocked-domain.com")) {
             throw BadRequestException("error.blocked_domain")
         }
     }
